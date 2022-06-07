a72c91a6cfd5e523788c36654492062ca39df413
Ilyas Taoussi <ilyas.taoussi2000@gmail.com>
2022-06-07 11:41:32 +0200

    feat(Tab): - Save Solution to file in resources/output folder - Refactor Filename to instructed name

--- src/ex1/Tab.java
+++ src/ex1/Tab.java
@@ -5,10 +5,13 @@ import ilog.concert.IloLinearNumExpr;
 import ilog.concert.IloNumVar;
 import ilog.cplex.IloCplex;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
+
+/**
+ * @author Ilyas Taoussi, Birkan Yildiz
+ * @version 1.0
+ */
 
 public class Tab {
     public static int[][] createTab(String path) throws FileNotFoundException {
@@ -17,10 +20,10 @@ public class Tab {
         String ligne = sc.nextLine();
         int[][] tab = new int[Integer.parseInt(ligne.split(" ")[0])][Integer.parseInt(ligne.split(" ")[1])];
         int i = 0;
-        while(sc.hasNextLine()) {
+        while (sc.hasNextLine()) {
             ligne = sc.nextLine();
             var tmp = ligne.split(" ");
-            for(int j = 0; j < tmp.length; j++) {
+            for (int j = 0; j < tmp.length; j++) {
                 tab[i][j] = Integer.parseInt(tmp[j]);
             }
             i++;
@@ -28,12 +31,12 @@ public class Tab {
         return tab;
     }
 
-    public static Graph tabToGraph(int[][] tab){
-        int index=0;
+    public static Graph tabToGraph(int[][] tab) {
+        int index = 0;
         Graph g = new Graph();
-        for(int x=0; x<tab.length; x++){
-            for(int y=0; y<tab[x].length; y++){
-                if(tab[x][y] != 0){
+        for (int x = 0; x < tab.length; x++) {
+            for (int y = 0; y < tab[x].length; y++) {
+                if (tab[x][y] != 0) {
                     Sommet s = new Sommet(index, new Position(x, y), tab[x][y]);
                     g.addSommet(s);
                 }
@@ -41,15 +44,15 @@ public class Tab {
             }
         }
         g.setMaxIndex(index);
-        for(Sommet s1: g.getGraph().keySet()) {
-            for(Sommet s2: g.getGraph().keySet()){
+        for (Sommet s1 : g.getGraph().keySet()) {
+            for (Sommet s2 : g.getGraph().keySet()) {
                 if (!s1.equals(s2) && s1.distanceTo(s2) != -1) g.addArete(s1.getIndex(), s2.getIndex());
             }
         }
         return g;
     }
 
-    public static void solveModel(Graph graph) throws IloException {
+    public static void solveModel(Graph graph, String path) throws IloException, IOException {
         int n = graph.getMaxIndex();
         int s = graph.getSourceIndex();
         int t = graph.getTargetIndex();
@@ -59,65 +62,82 @@ public class Tab {
         IloNumVar[][] x = new IloNumVar[n][];
         double[][] c = new double[n][n];
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             x[i] = model.boolVarArray(n);
         }
 
-        for(Sommet sommet: graph.getGraph().keySet()){
+        for (Sommet sommet : graph.getGraph().keySet()) {
             var aretes = graph.getGraph().get(sommet);
-            for(Arete arete : aretes){
+            for (Arete arete : aretes) {
                 c[arete.getSommet1()][arete.getSommet2()] = arete.getCost();
             }
         }
 
         IloLinearNumExpr obj = model.linearNumExpr();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) {
-                if(c[i][j] != 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (c[i][j] != 0) {
                     obj.addTerm(c[i][j], x[i][j]);
                 }
             }
         }
         model.addMinimize(obj);
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             IloLinearNumExpr constraint = model.linearNumExpr();
-            for(int j = 0; j < n; j++){
-                if(c[i][j] != 0) {
+            for (int j = 0; j < n; j++) {
+                if (c[i][j] != 0) {
                     constraint.addTerm(1.0, x[i][j]);
                     constraint.addTerm(-1.0, x[j][i]);
                 }
             }
-            if(i == s) model.addEq(constraint, 1.0);
-            else if(i == t) model.addEq(constraint, -1.0);
+            if (i == s) model.addEq(constraint, 1.0);
+            else if (i == t) model.addEq(constraint, -1.0);
             else model.addEq(constraint, 0.0);
         }
 
         boolean isSolved = model.solve();
-        if(isSolved){
+        if (isSolved) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+            StringBuilder sb = new StringBuilder();
             double objValue = model.getObjValue();
-            System.out.println("Obj_val = " + objValue);
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(c[i][j] != 0) {
+            sb.append("Obj_val (min Distance) = ")
+                    .append(objValue).append("\n")
+                    .append("Chemin :").append("\n")
+                    .append("[");
+            int i = s;
+            while (i != t) {
+                for (int j = 0; j < n; j++) {
+                    if (c[i][j] != 0) {
                         var val = model.getValue(x[i][j]);
-                        if(val != 0)System.out.println("x[" + (i+1) + "]["+ (j+1) + "]= " + val);
+                        if (val != 0) {
+                            if (j == t)
+                                sb.append(i).append(" ").append(graph.getSommet(i).getPosition()).append(" -> ").append(j).append(graph.getSommet(j).getPosition()).append("]");
+                            else sb.append(i).append(graph.getSommet(i).getPosition()).append(" -> ");
+                            i = j;
+                            break;
+                        }
                     }
                 }
             }
-        } else{
-          System.out.println("No Solution");
+            writer.append(sb.toString());
+            writer.flush();
+            writer.close();
+        } else {
+            System.out.println("No Solution");
         }
     }
 
     public static void main(String[] args) {
-        try{
-            var tab = createTab("resources/examples/reseau_5_10_1.txt");
+        try {
+            String path = "resources/examples/reseau_50_50_1.txt";
+            String sol_path = "resources/output/sol_" + path.split("/")[2];
+
+            var tab = createTab("resources/examples/reseau_50_50_1.txt");
             Graph g = tabToGraph(tab);
-            for(int[] t : tab)
-                System.out.println(Arrays.toString(t));
-            solveModel(g);
-        }catch (IloException | FileNotFoundException ex){
+
+            solveModel(g, sol_path);
+        } catch (IloException | IOException ex) {
             ex.printStackTrace();
         }
 
